b06938ed47a13e29711e26791bf046bffb4ea394
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-09 14:18:08 +0400

    Create MixinMinecraft.java

---
+++ src/main/java/de/vepexlegit/clickgui/mixins/MixinMinecraft.java
@@ -0,0 +1,16 @@
+package de.vepexlegit.clickgui.mixins;
+
+import net.minecraft.client.Minecraft;
+import org.lwjgl.opengl.Display;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+    @Inject(method = "createDisplay", at = @At("RETURN"))
+    private void createDisplay(final CallbackInfo ci) {
+        Display.setTitle("Test Title");
+    }
+}
